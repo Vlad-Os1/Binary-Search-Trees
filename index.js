f18972442e6a14c5741ea5f9d760d8702ff9c813
Vlad_Os <oshovskiyvlad28@gmail.com>
2025-07-04 02:07:32 +0300

    feat: add find and delete methods - add find method - add incomplete delete method which just finds parent element for our node to delete

--- index.js
+++ index.js
@@ -46,6 +46,39 @@ class Tree {
       }
     }
   }
+  deleteItem(value) {
+    let nodeToDelete = this.find(value);
+    let current = this.root;
+    let parent = this.root;
+    while (parent !== null) {
+      if (parent.right === nodeToDelete || parent.left === nodeToDelete) {
+        break;
+      }
+      if (nodeToDelete.data > parent.data) {
+        parent = parent.right;
+      } else {
+        parent = parent.left;
+      }
+    }
+    console.log(parent);
+  }
+  find(value) {
+    if (this.root === null) {
+      return null;
+    }
+    let current = this.root;
+    while (current !== null) {
+      if (value === current.data) {
+        return current;
+      }
+      if (value < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    throw new Error('This value is not in the tree');
+  }
 }
 
 const prettyPrint = (node, prefix = '', isLeft = true) => {
@@ -65,3 +98,4 @@ let array = [0, 1, 1, 2, 9, 3, 10, 4, 5, 6];
 let tree = new Tree(array);
 tree.insert(7);
 prettyPrint(tree.root);
+console.log(tree.deleteItem(5));
